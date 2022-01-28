acd4d60435bbf331fbd5acc8b82f4c9bcf7ac053
Nirav Maisuria <64515375+niravautomation30@users.noreply.github.com>
2022-01-28 18:12:53 -0500

    add comment to Test_2

--- PracticeCND/src/Test_2.java
+++ PracticeCND/src/Test_2.java
@@ -4,7 +4,8 @@ public class Test_2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		// this is test_2 comment
+		// this is test_2 comment here
+		// this is test_3 comment
 	}
 
 }
