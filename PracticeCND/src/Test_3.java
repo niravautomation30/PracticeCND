f518760f1bc5a854ebb29e6acacaf7a8cef0d1bc
Nirav Maisuria <64515375+niravautomation30@users.noreply.github.com>
2022-01-29 16:03:27 -0500

    add comment in

--- PracticeCND/src/Test_3.java
+++ PracticeCND/src/Test_3.java
@@ -5,10 +5,10 @@ public class Test_3 {
 		// TODO Auto-generated method stub
 
 		System.out.println("Hello");
-		int a1=10;
-		int a2=10;
-		int a3=10;
-		int a4=10;
+		int a1=10; // add a1
+		int a2=10; // add a2
+		int a3=10; // add a3
+		int a4=10; // add a4
 	}
 
 }
